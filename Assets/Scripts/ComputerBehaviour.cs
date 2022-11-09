952cd264805c5fb25f7fa0d63dd181a2171dfb17
Betul Gures <fatma.betul.gures@gmail.com>
2022-11-09 18:21:22 +0300

    bugfix

--- Assets/Scripts/ComputerBehaviour.cs
+++ Assets/Scripts/ComputerBehaviour.cs
@@ -3,12 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ComputerBehaviour : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _textTargetEnglish, _textTargetGerman;
-    [SerializeField] private GameObject _productTargetImage, _audioPlayerEn, _audioPlayerDe;
-    private AudioSource sound; 
+    [SerializeField] private Image _productTargetImage;
+    [SerializeField] private AudioSource _audioPlayerEn, _audioPlayerDe;
+    private AudioSource sound;
+    private bool isPressed;
+    private GameObject presser;
 
     private void Start()
     {
@@ -17,12 +21,24 @@ public class ComputerBehaviour : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent(out SuperMarketGoods _superMarketGoods))
+        if ((!isPressed)&&(other.TryGetComponent(out SuperMarketGoods _superMarketGoods)))
         {
             sound.Play();
+            presser = other.gameObject;
             _textTargetEnglish.text = _superMarketGoods._nameEnglish;
             _textTargetGerman.text = _superMarketGoods._nameGerman;
-
+            _productTargetImage.sprite = _superMarketGoods._productImage;
+            _audioPlayerEn.clip = _superMarketGoods._pronounciaitonEnglish;
+            _audioPlayerEn.clip = _superMarketGoods._pronounciaitonGerman;
         }
     }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == presser)
+        {
+            isPressed = false;
+        }
+    }
+    
 }
---
+++ Assets/Scripts/MoveGoods.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGoods : MonoBehaviour
+{
+    [SerializeField] Rigidbody[] goods;
+    
+    public void MovingGoods()
+    {
+        foreach (var good in goods)
+        {
+            good.AddForce(Vector3.forward * 0.1f, ForceMode.Impulse);
+        }
+    }
+    
+}
